ce22b5dc5c97a4c035e71c2325763546067368c6
kn1ghtich <149316397+kn1ghtich@users.noreply.github.com>
2024-01-04 22:52:35 +0600

    dawda

--- 1.py
+++ 1.py
@@ -5,7 +5,7 @@ a = float(input("Enter the side of square: "))
 if a % 1 == 0:
     a = int(a)
 p = 4*a
-s = a*a
-print("The perimeter of square = " + p)
-print("The area of square = " + a)
+s = a*a   
+print("The perimeter of square = " + str(p))
+print("The area of square = " + str(a))
 
